2763dc524fe305a48a36331cb621908631b8d3e9
Yariazen <35210313+Yariazen@users.noreply.github.com>
2024-09-24 02:03:22 -0700

    Revert logging

--- Bridge.java
+++ Bridge.java
@@ -1,5 +1,3 @@
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Paths;
@@ -7,27 +5,15 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 class Handler implements URLHandler {
-    String currentDir;
-    String logFilePath;
-    
-    public Handler(String currentDir, String logFilePath) {
-        this.currentDir = currentDir;
-        this.logFilePath = logFilePath;
-    }
+    String currentDir = Paths.get("").toAbsolutePath().toString();
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/start-server")) {
             String scriptPath = currentDir + "\\start-server.ps1";
-
-            String response = handleScript(scriptPath);
-            logToFile(response);
-            return response;
+            return handleScript(scriptPath);
         } else if (url.getPath().equals("/update-server")) {
             String scriptPath = currentDir + "\\update-server.ps1";
-            
-            String response = handleScript(scriptPath);
-            logToFile(response);
-            return response;
+            return handleScript(scriptPath);
         } else {
             return "404 Missing";
         }
@@ -62,31 +48,13 @@ class Handler implements URLHandler {
             return "Encountered unexpected error.";
         }
     }
-
-    private void logToFile(String message) {
-        try (FileWriter writer = new FileWriter(logFilePath, true)) { // Append mode
-            writer.write(message + "\n");
-        } catch (IOException e) {
-            System.err.println("Failed to write to log file: " + e.getMessage());
-        }
-    }
 }
 
 class Bridge {
 
     public static void main(String[] args) throws IOException {
         int port = 8080;
-        String currentDir = Paths.get("").toAbsolutePath().toString();
-        String logFilePath = currentDir + "\\log.log";
-        String previousLogFilePath = currentDir + "\\previous.log";
 
-        File currentLogFile = new File(logFilePath);
-        if (currentLogFile.exists()) {
-            currentLogFile.renameTo(new File(previousLogFilePath));
-        }
-
-        currentLogFile.createNewFile();
-
-        Server.start(port, new Handler(currentDir, logFilePath));
+        Server.start(port, new Handler());
     }
 }
