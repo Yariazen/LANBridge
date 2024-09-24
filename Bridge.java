7eff07e2317bdb984773459ab1f1e40dbcf3f4ee
Yariazen <35210313+Yariazen@users.noreply.github.com>
2024-09-24 01:10:50 -0700

    handleScript

--- Bridge.java
+++ Bridge.java
@@ -1,32 +1,48 @@
 import java.io.IOException;
 import java.net.URI;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 class Handler implements URLHandler {
-    public List<String> messages = new ArrayList<String>();
+    String currentDir = Paths.get("").toAbsolutePath().toString();
     
     public String handleRequest(URI url) {
-        if (url.getPath().equals("/add-message")) {
-            String[] args = url.getQuery().split("&");
-            for (String arg : args) {
-                String[] kvp = arg.split("=");
-                String message = null;
-                String user = null;
-                if (kvp[0].equals("message")) {
-                    message = kvp[1];
-                } else if (kvp[0].equals("user")) {
-                    user = kvp[1];
-                }
-                if (message != null && user != null) {
-                    messages.add(user + ": " + message);
-                }
-            }
-            return String.join("\n", messages);
+        if (url.getPath().equals("/start-server")) {
+            String scriptPath = currentDir + "\\start-server.ps1";
+            return handleScript(scriptPath);
+        } if else (url.getPath().equals("/update-server")) {
+            String scriptPath = currentDir + "\\update-server.ps1";
+            return handleScript(scriptPath);
         } else {
             return "404 Missing";
         }
+    }
+
+    public String handleScript(String scriptPath) {
+        String command = "powershell.exe \"" + scriptPath + "\"";
+        Process powerShellProcess = Runtime.getRuntime().exec(command);
+        powerShellProcess.getOutputStream().close();
+
+        StringBuilder str = new StringBuilder();
+        String line;
+
+        str.append("Standard Output:\n");
+        BufferedReader stdout = new BufferedReader(new InputStreamReader(powerShellProcess.getInputStream()));
+        while ((line = stdout.readLine()) != null) {
+            str.append(line).append("\n");
+        }
+        stdout.close();
+
+        str.append("Standard Error:\n");
+        BufferedReader stderr = new BufferedReader(new InputStreamReader(powerShellProcess.getErrorStream()));
+        while ((line = stderr.readLine()) != null) {
+            str.append(line).append("\n");
+        }
+        stderr.close();
+
+        str.append("Done\n");
+        return str.toString();
     }
 }
 
